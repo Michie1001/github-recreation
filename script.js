c8966b4fe1bbace03bea9c3fe77a97154049f0a5
Michie1001 <michieugwu@gmail.com>
2020-11-29 23:05:00 +0100

    curl

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-curl -H "Authorization: bearer 058b2574437411b3b03286bbc1dcf564abf61831" -X POST -d " \
- { \
-   \"query\": \"query { viewer { login }}\" \
- } \
-" https://api.github.com/graphql
+console.log("ole");
