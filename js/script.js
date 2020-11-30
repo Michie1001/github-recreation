504f006436bea1c4f2e61fa2c6d0650ecd7a17ed
Michie1001 <michieugwu@gmail.com>
2020-11-30 15:10:45 +0100

    JSON would make me run mad, amen

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Authorization", "Bearer " + token);
 
 var graphql = JSON.stringify({
-  query: "query($username: String!) {\n    user(login: $username) {\n        name\n        repositories(last: 20){\n            edges{\n                node{\n                    name\n                    description\n                    updatedAt\n                }\n            }\n        }\n    }\n}",
+  query: "query($username: String!) {\n user(login: $username) {\n name\n repositories(last: 20){\n edges{\n node{\n name\n description\n updatedAt\n}\n}\n}\n}\n}",
   variables: {"username":"Michie1001"}
 })
 var requestOptions = {
@@ -20,7 +20,11 @@ var requestOptions = {
 
 fetch("https://api.github.com/graphql", requestOptions)
   .then(response => response.text())
-  .then(result => console.log(result))
+  .then(result => {
+  	document.getElementById('repos').innerHTML = JSON.stringify(JSON.parse(result)); //Just to display on the page
+  })
   .catch(error => console.log('error', error));
 
-document.getElementById('repos').innerHTML = requestOptions;
+// document.getElementById('repos').innerHTML = JSON.stringify(result);
+
+//   .then(result => console.log(result))
