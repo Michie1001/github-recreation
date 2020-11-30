4eb496496bc928eebe5b39fb9f19ac35abbb9984
Michie1001 <michieugwu@gmail.com>
2020-11-30 14:25:11 +0100

    hidden the token key

--- graphql.js
+++
@@ -1,7 +0,0 @@
-curl -H "Authorization: bearer token" -X POST -d " \
- { \
-   \"query\": \"query { viewer { login }}\" \
- } \
-" https://api.github.com/graphql
-
-058b2574437411b3b03286bbc1dcf564abf61831
---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Using Fetch to call the API
+
+
+var token = config.MY_TOKEN; //Hidden token, of course
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Bearer " + token);
+
+var graphql = JSON.stringify({
+  query: "query($username: String!) {\n    user(login: $username) {\n        name\n        repositories(last: 20){\n            edges{\n                node{\n                    name\n                    description\n                    updatedAt\n                }\n            }\n        }\n    }\n}",
+  variables: {"username":"Michie1001"}
+})
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: graphql,
+  redirect: 'follow'
+};
+
+fetch("https://api.github.com/graphql", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+document.getElementById('repos').innerHTML = requestOptions;
